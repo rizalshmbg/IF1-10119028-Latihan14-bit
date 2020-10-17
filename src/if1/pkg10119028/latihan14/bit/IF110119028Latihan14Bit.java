02251e70c7032e5634096d8d1790b873cd95045f
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-10-18 01:24:12 +0700

    Add files via upload

---
+++ src/if1/pkg10119028/latihan14/bit/IF110119028Latihan14Bit.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan14.bit;
+
+/**
+ *
+ * @author
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk
+ */
+public class IF110119028Latihan14Bit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 60; /* 60 = 0011 1100 */
+        int b = 13; /* 30 = 0000 1101 */
+        int c = 0;
+        c = a & b;
+        /* 12 - 0000 1100 */
+        System.out.println("a & b = " + c);
+        c = a | b;
+        /* 61 = 0011 1101 */
+        System.out.println("a | b = " + c);
+        c = a ^ b;
+        /* 49 = 0011 0001 */
+        System.out.println("a ^ b = " + c);
+        c = ~a;
+        /* -61 = 1100 0011 */
+        System.out.println("~a = " + c);
+        c = a << 2;
+        /* 240 - 1111 0000 */
+        System.out.println("a << 2 = " + c);
+        c = a >> 2;
+        /* 15 = 0000 1111 */
+        System.out.println("a >> 2 = " + c);
+    }
+    
+}
